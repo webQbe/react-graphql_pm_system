7c6ed59ac2ace41dea89467021f1f91c7c352f52
webQbe <webcubesolutionslk@gmail.com>
2025-06-12 14:52:38 +0530

    Created React app for frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default App
