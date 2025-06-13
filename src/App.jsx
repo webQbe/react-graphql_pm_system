6d5f0618d83ba2d30a87d003f22b4d28dbb8ee8a
webQbe <webcubesolutionslk@gmail.com>
2025-06-13 19:05:13 +0530

    Configured ApolloClient cache behavior using typePolicies

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,38 @@ import Header from './components/Header'
 import Clients from './components/Clients'
 import './App.css' // for global and component styles
 
+// Replace existing data with incoming data 
+const cache = new InMemoryCache({ // InMemoryCache:	Apollo's built-in caching system
+  
+  // typePolicies:	Customize how Apollo stores and merges specific types/fields
+  typePolicies: { 
+    Query: {
+      fields: {
+        clients: {
+          // Control how Apollo cache merges new data into its existing cache
+          merge(existing, incoming){ 
+            return incoming // overriding merge() to return only new data instead of merging
+          }
+        },
+        projects: {
+          merge(existing, incoming){
+            return incoming 
+            /* By overriding the merge function and returning only incoming, you ensure:
+                1. Every time you query `clients` or `projects`, you get a fresh and accurate list.
+                2. Prevents weird bugs when deleting or updating items. 
+            */
+
+          }
+        }
+      }
+    }
+  }
+})
+
 /* Set up ApolloClient to talk to GraphQL server */
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql', // backend GraphQL endpoint
-  cache: new InMemoryCache()            // enable ApolloClient caching for efficient reuse and re-rendering
+  cache // Call whenever a new response is fetched         
 })
 
 function App() {
