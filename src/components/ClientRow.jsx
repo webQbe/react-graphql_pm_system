1821d75f87215db2995ad1e1e701f88d51e3c56b
webQbe <webcubesolutionslk@gmail.com>
2025-06-16 18:54:19 +0530

    After successful deletion of a client, re-fetched projects & re-rendered UI

--- src/components/ClientRow.jsx
+++ src/components/ClientRow.jsx
@@ -3,6 +3,7 @@ import { FaTrash } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
 import { DELETE_CLIENT } from '../mutations/clientMutations'
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries'
 
 const ClientRow = ({ client }) => { // Receive client object as a prop
 
@@ -11,12 +12,25 @@ const ClientRow = ({ client }) => { // Receive client object as a prop
                                       // Execute DELETE_CLIENT mutation using current client.id
                                       DELETE_CLIENT, {
                                       variables: { id: client.id }, // Pass ID of the client to be deleted
+                                      refetchQueries: [
+                                                        { query: GET_CLIENTS }, 
+                                                        { query: GET_PROJECTS } //  Re-run the GET_PROJECTS query after DELETE_CLIENT mutation succeeds
+                                                      ], 
+                                      /* Deleted client also deletes projects 
+                                         - Apollo’s cache might still show stale data (e.g., orphaned projects or broken client fields).
+                                         - Refetching ensures that the UI reflects the current state of both clients and projects.                
+                                          
+                                         a best practice when:
+                                         - The mutation has side effects on other parts of the cached data.
+                                         - You want to avoid manually updating the cache after a mutation.  
+                                      */
+
                                       // Trigger custom function to directly manipulate Apollo’s normalized cache
-                                      update(cache, { data: { deleteClient } }) {
+                                      /* update(cache, { data: { deleteClient } }) {
                                       /* This updates the UI immediately without waiting for a round trip to the server
                                         - Backend client-deletion is done by useMutation 
                                         - Frontend-cache-update is done by this update() method to reflect the changes immediately in the UI
-                                      */
+                                      
                                       try  { 
                                             const { clients } = cache.readQuery({ query: GET_CLIENTS }) // Read existing clients data from cache
                                             // Write new list of clients to cache
@@ -32,7 +46,7 @@ const ClientRow = ({ client }) => { // Receive client object as a prop
 
                                               console.warn('Cache not ready for GET_CLIENTS');
                                           }    
-                                      },
+                                      }, */
                                     })
 
   return (
